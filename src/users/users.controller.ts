c694d890e3c62a96324922085910017a5cb1b0e2
Darren Messenger <39158691+darrenmessenger@users.noreply.github.com>
2022-04-05 08:47:48 +0100

    Main functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UserService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/me')
+  me(@Req() request) {
+    const userId = request.user.userId;
+    return this.userService.findOne(userId);
+  }
 }
